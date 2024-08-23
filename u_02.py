d714c05f8807d6550cee8933b954a5944c9fe480
Jeff.kim <125433456+vkfkdto004@users.noreply.github.com>
2024-08-23 16:30:47 +0900

    Update u_02.py

--- u_02.py
+++ u_02.py
@@ -3,49 +3,52 @@
 import os
 import subprocess
 
-print('--------------------------U-02 패스워드 복잡성 설정 진단을 실시합니다.-------------------------')
-
-os = input(str('os 정보를 입력해주세요: '))
-version = input(str('version: 정보를 입력해주세요(ubuntu는 .을 빼고 입력): '))
+print('--------------------------U-02 패스워드 복잡성 설정 진단을 실시합니다.-------------------------\n')
 
 def check_file(path, filename):
     try:
-        file_check = subprocess.check_output(f'ls -al {path} 2> /dev/null', shell=True, text=True)
+        file_check = subprocess.check_output(f'ls -al {path} 2> /dev/null', shell=True, text=True)        
         if filename in file_check:
             print(f'{path} 파일을 확인했습니다.')
+            return True
         else:
             print(f'{path} 파일이 없습니다. os와 version을 다시 입력해주세요.')
+            return False
     except subprocess.CalledProcessError:
         print(f'{path} 파일을 확인할 수 없습니다.\n')
 
+def test():
 # OS 및 버전 정보에 따른 파일 확인 로직
-version = int(version)
-need_check = False
+    os = input(str('os 정보를 입력해주세요: '))
+    version = input(str('version: 정보를 입력해주세요(ubuntu는 .을 빼고 입력): '))
+    need_check = False
 
-if os == "solaris" and version >= 10:
-    check = check_file('/etc/default/passwd', 'passwd')
-    if check:
-        need_check = True
-elif os == "centos" and version >= 5 and version < 7:
-    check = check_file('/etc/pam.d/system-auth', 'system-auth')
-    if check:
-        need_check = True
-elif os == "centos" and version >= 7:
-    check = check_file('/etc/security/pwquality.conf', 'pwquality.conf')
-    if check:
-        need_check = True
-elif os == "ubuntu":
-    check = check_file('/etc/security/pwquality.conf', 'pwquality.conf')
-    if check:
-        need_check = True
-else:
-    print("지원되지 않는 OS 또는 버전입니다. os와 version을 다시 입력해주세요.")
+    if os == "solaris" and version >= '10':
+        solaria_check = check_file('/etc/default/passwd', 'passwd')
+        if solaria_check:
+            need_check = True
+            return need_check
+    elif os == "centos" and version >= '5' and version < '7':
+        centos5_check = check_file('/etc/pam.d/system-auth', 'system-auth')
+        if centos5_check:
+            need_check = True
+            return need_check
+    elif os == "centos" and version >= '7':
+        centos7_check = check_file('/etc/security/pwquality.conf', 'pwquality.conf')
+        if centos7_check:
+            need_check = True
+            return need_check
+    elif os == "ubuntu":
+        ubuntu_check = check_file('/etc/security/pwquality.conf', 'pwquality.conf')
+        if ubuntu_check:
+            need_check = True
+            return need_check
+    else:
+        print("지원되지 않는 OS 또는 버전입니다. os와 version을 다시 입력해주세요.")
 
 # /etc/shadow 비밀번호가 설정되어 있는지 안되어 있는지 확인해야함
 
-
-
-
+need_check = test()
 # 패스워드 정책 설정
 if need_check:
         print("================================================================================")
@@ -57,7 +60,7 @@ if need_check:
         print("minlen: 최소 패스워드 길이 설정입니다. 최소 8자리 이상 요구됩니다.")
         print(" ")
         print(" ")
-        print("=========================패스워드 설정을 확인합니다===============================" )
+        print("=========================패스워드 설정을 확인합니다===============================" )      
         print("현재 설정되어 있는 항목들입니다.\n")
         confirm_setting = ['lcredit', 'ucredit', 'dcredit','ocredit','minlen']
         for setting in confirm_setting:
