6abefa772fd75f4bd0214e5c3b87258d4b4a91a0
Jeff.kim <125433456+vkfkdto004@users.noreply.github.com>
2024-08-23 17:51:25 +0900

    Update u_01.py

--- u_01.py
+++ u_01.py
@@ -3,7 +3,7 @@
 import os
 import subprocess
 
-print('U-01: root 계정 원격접속 제한')
+print('\nU-01: root 계정 원격접속 제한\n')
 
 sshd_service_alive = False
 telnet_service_alive = False
@@ -20,35 +20,37 @@ if bool(telnet_service):
     print("Telnet 서비스  활성화 중입니다\n")
 
 # SSH서비스  진단  실시
-print("#################SSH 서비스 진단 시작합니다.############################")
-print("========================================================================")
-dig_ssh_service = subprocess.check_output('cat /etc/ssh/sshd_config | grep -i "#permitrootlogin"', shell=True, text=True)   
-if '#' in dig_ssh_service:
-    print("\n####/etc/ssh/sshd_config####\n!!!!!!!설정이 취약합니다.!!!!!!!!!!!!!\n")
-else:
-    print("\n####/etc/ssh/sshd_config####\n!!!!!!!설정이 양호합니다.!!!!!!!!!!!!!\n")
-
-
-# telnet 서비스 진단 실시
-print("#################Telnet 서비스 진단 시작합니다.#########################")
+print("\n#################SSH 서비스 진단 시작합니다.############################")
 print("========================================================================")
 try:
-    dig_telnet_service = subprocess.check_output('cat /etc/pam.d/login | grep -i "pam_securetty.so"', shell=True, text=True)
+    dig_ssh_service = subprocess.check_output('cat /etc/ssh/sshd_config | grep -i "#permitrootlogin"', shell=True, text=True)
+    if '#' in dig_ssh_service:
+        print("\n####/etc/ssh/sshd_config####\n!!!!!!!설정이 취약합니다.!!!!!!!!!!!!!\n")
+    else:
+        print("\n####/etc/ssh/sshd_config####\n!!!!!!!설정이 양호합니다.!!!!!!!!!!!!!\n")
+except subprocess.CalledProcessError:
+    print('조치가 완료되었습니다.')
+
+# telnet 서비스 진단 실시
+print("\n#################Telnet 서비스 진단 시작합니다.#########################")
+print("========================================================================")
+try:
+    dig_telnet_service = subprocess.check_output('cat /etc/pam.d/login | grep -i "pam_securetty.so"', shell=True, text=True) 
     print("\n####/etc/pam.d/login####\n!!!!!!!설정이 양호합니다.!!!!!!!!!!!!!\n")
 except subprocess.CalledProcessError:
     print("\n####/etc/pam.d/login####\n!!!!!!!설정이 취약합니다.!!!!!!!!!!!!!\n")
 
 # 사용자 선택권에 대한 코드를 추가해서 진행한다. -> 내부적으로 문제가 될 수가 있다.
 # SSH, Telnet 조치 취하기
-print("---------------SSH 서비스 취약점 조치 합니다.-------------------")
-clear_ssh_service = subprocess.check_output('cat /etc/ssh/sshd_config | grep -i "#permitrootlogin"', shell=True, text=True) 
-if 'prohibit-password' in clear_ssh_service:
-    os.system('sed "s/#PermitRootLogin prohibit-password/PermitRootLogin No/g" /etc/ssh/sshd_config')
+#print("---------------SSH 서비스 취약점 조치 합니다.-------------------")
+#clear_ssh_service = subprocess.check_output('cat /etc/ssh/sshd_config | grep -i "#permitrootlogin"', shell=True, text=True) 
+#if 'prohibit-password' in clear_ssh_service:
+#    os.system('sed "s/#PermitRootLogin prohibit-password/PermitRootLogin No/g" /etc/ssh/sshd_config')
 
 
-print("----------조치 완료했습니다. 다시 검사합니다---------------------")
-dig_ssh_service = subprocess.check_output('cat /etc/ssh/sshd_config | grep -i "#permitrootlogin"', shell=True, text=True)   
-if 'PermitrootLogin No' in dig_ssh_service:
-    print("\n####/etc/ssh/sshd_config####\n!!!!!!!설정이 취약합니다.!!!!!!!!!!!!!\n")
-else:
-    print("\n####/etc/ssh/sshd_config####\n!!!!!!!설정이 양호합니다.!!!!!!!!!!!!!\n")
+#print("----------조치 완료했습니다. 다시 검사합니다---------------------")
+#dig_ssh_service = subprocess.check_output('cat /etc/ssh/sshd_config | grep -i "#permitrootlogin"', shell=True, text=True)   
+#if 'PermitrootLogin No' in dig_ssh_service:
+#    print("\n####/etc/ssh/sshd_config####\n!!!!!!!설정이 취약합니다.!!!!!!!!!!!!!\n")
+#else:
+#    print("\n####/etc/ssh/sshd_config####\n!!!!!!!설정이 양호합니다.!!!!!!!!!!!!!\n")
